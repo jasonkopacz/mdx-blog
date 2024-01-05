8d31476c285f482fd9bb292efff16aa2e1610ca1
Jason <jason@Arielles-MacBook-Pro.local>
2024-01-04 19:01:22 -0500

    metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -8,9 +8,18 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import { loadBlogPost } from '@/helpers/file-helpers';
 
+export async function generateMetadata({ params }) {
+  const post = await loadBlogPost(params.postSlug)
+
+  return {
+    title: `${post.frontmatter.title}`,
+    name: `${post.frontmatter.abstract}`,
+    content: `${post.content}`
+  }
+}
+
 export default async function BlogPost({ params }) {
-  const slug = params.postSlug
-  const post = await loadBlogPost(slug)
+  const post = await loadBlogPost(params.postSlug)
 
   return (
     <article className={styles.wrapper}>
--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,7 @@ import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './styles.css';
+import { BLOG_TITLE } from '@/constants';
 
 const mainFont = Work_Sans({
   subsets: ['latin'],
@@ -23,6 +24,12 @@ const monoFont = Spline_Sans_Mono({
   weight: 'variable',
   variable: '--font-family-mono',
 });
+
+export const metadata = {
+  title: BLOG_TITLE,
+  name: 'Jason Kopacz Blog',
+  content: 'A blog teaching topics spanning throughout the full stack'
+}
 
 function RootLayout({ children }) {
   // TODO: Dynamic theme depending on user preference
